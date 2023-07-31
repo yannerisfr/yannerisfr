cc745cc7f7a381deef5e27390bb4dd2489337e65
adferrinm <62492197+adferrinm@users.noreply.github.com>
2023-07-31 11:59:31 -0400

    fix padding and url

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,6 @@ import image from "@astrojs/image";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yannerisfr.github.io/',
-  base: '/yannerisfr',
   integrations: [mdx(), sitemap(), tailwind(), image(
     {
       serviceEntryPoint: '@astrojs/image/sharp',
